85bcdef03e6a97c8543226ab167b740433b2f149
MohamedOsoble <Mohamed.Osoble@outlook.com>
2024-09-10 17:42:06 +0100

    add resize and needsResize functionality

---
+++ hash-maps.mjs
@@ -0,0 +1,137 @@
+import LinkedList from "./linked-list.mjs";
+
+export default class HashMap {
+
+    constructor(){
+        this.capacity = 16;
+        this.size = 0;
+        this.loadFactor = 0.75
+        this.buckets = this.initializeBuckets()
+    };
+
+    initializeBuckets(){
+        return Array.from({ length: this.capacity }, () => new LinkedList());
+    }
+
+    needsResize(){
+        return this.size > this.capacity * this.loadFactor;
+    };
+
+    resize(){
+        const oldBuckets = this.buckets;
+        this.capacity *= 2;
+        this.buckets = this.initializeBuckets;
+        for(const bucket of oldBuckets){
+            let node = bucket.head;
+            while(node){
+                this.set(node.key, node.value)
+                node = node.next;
+            }
+        }
+    };
+
+    hash(key){
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for(let i = 0; i < key.length; i++){
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        };
+        return hashCode
+    };
+
+    getBucket(key){
+        return this.buckets[this.hash(key)]
+    }
+
+    set(key, value){
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+        let node = bucket.head;
+
+        // Loop over all nodes while it's not empty, if an identical key is found, update the value
+        while(node) {
+            if(node.key === key){
+                node.key = key
+                node.value = value;
+                return;
+            };
+            node = node.next;
+        }
+
+        // if not found, update the final node and add the K,V pair
+        bucket.append(key, value);
+        this.size++;
+
+        // add functionality to resize buckets later
+    };
+
+    get(key){
+
+        // get bucket location
+        const bucket = getBucket(key)
+        return bucket.at(bucket.findKey(key))
+    };
+
+    has(key){
+        const bucket = getBucket(key);
+        return bucket.findKey(key);
+
+    };
+
+    remove(key){
+        let bucket = get(key);
+        if(bucket){
+            bucket.pop(bucket.find(get(key)));
+            this.size--
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    };
+
+    length(){
+        return this.size
+    };
+
+    clear(){
+        let oldBuckets = this.buckets;
+        this.capacity = 16;
+        this.size = 0;
+        this.buckets = this.initializeBuckets
+        return true;
+    };
+
+    keys(){
+        let keys = [];
+        for(let i = 0; i < this.capacity -1; i++){
+            let bucket = this.buckets[i]
+            keys.push(bucket.iterateNodes("keys"));
+        }
+        return keys;
+    };
+
+    values(){
+        let values = [];
+        for(let i = 0; i < this.capacity -1; i++){
+            let bucket = this.buckets[i]
+            values.push(bucket.iterateNodes("values"));
+        }
+        return values;
+    };
+
+    entries(){
+        let entries = [];
+        for(let i = 0; i < this.capacity -1; i++){
+            let bucket = this.buckets[i]
+            let entry = bucket.iterateNodes("entries")
+            if(entry.length > 0){
+                entries.push(entry)
+            }
+        }
+        return entries;
+    };
+};
---
+++ linked-list.mjs
@@ -0,0 +1,163 @@
+export default class LinkedList{
+
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = null
+    }
+
+
+    append(key, value){
+        const NODE = new Node()
+        NODE.value = value;
+        NODE.key = key
+        if(this.head == null){
+            this.head = NODE;
+            this.tail = NODE;
+            this.length++
+        }
+        else{
+            let pTail = this.tail;
+            pTail.next = NODE
+            this.tail = NODE
+            this.length++
+        }
+    };
+
+    prepend(key, value){
+        const NODE = new Node();
+        NODE.value = value;
+        NODE.key = key;
+        if(this.tail == null){
+            this.head = NODE;
+            this.tail = NODE;
+            this.length++
+        }
+        else{
+            let pHead = this.head
+            NODE.next = pHead;
+            this.head = NODE;
+            this.length++
+        };
+    };
+
+    at(index){
+        if(index >= this.length){
+            return "Index is out of bounds";
+        }
+        let currentNode = this.head
+        for(let i = 0; i < index; i++){
+            currentNode = currentNode.next;
+        };
+        return currentNode.value
+    };
+
+
+    pop(value){
+        let currentNode = this.head;
+        for(let i = 0; i < this.length; i++){
+            if(currentNode.next == value){
+                this.tail = currentNode
+                this.length--
+                return "popped"
+            }
+            else{
+                currentNode = currentNode.next;
+            };
+        };
+
+    };
+
+    contains(value){
+        let currentNode = this.head;
+        for(let i = 0; i < this.length; i++){
+            if(currentNode.value == value){
+                return true;
+            }
+            else{
+                currentNode = currentNode.next;
+            };
+        };
+        return false
+    };
+
+    find(value){
+        let index = null
+        let currentNode = this.head;
+        for(let i = 0; i < this.length; i++){
+            if(currentNode.value == value){
+                return i;
+            }
+            else{
+                currentNode = currentNode.next
+            }
+        }
+        return index
+    }
+
+    findKey(key) {
+        let currentNode = this.head;
+        let currentIndex = 0;
+
+        while (currentNode !== null) {
+          if (currentNode.key === key) {
+            return currentIndex;
+          };
+
+          currentNode = currentNode.next;
+          currentIndex++;
+        };
+
+        return -1;
+      };
+
+      iterateNodes(type){
+        let values = [];
+        if(this.length == 0){
+            return -1
+        }
+        if(type === "keys"){
+            let currentNode = this.head;
+            for(let i = 0; i < this.length; i++){
+                values.push(currentNode.value.KEY);
+                currentNode = currentNode.next;
+            };
+        }
+        else if(type === "values"){
+            let currentNode = this.head;
+            for(let i = 0; i < this.length; i++){
+                values.push(currentNode.value.VALUE);
+                currentNode = currentNode.next;
+            };
+        }
+        else{
+            let currentNode = this.head;
+            for(let i = 0; i < this.length; i++){
+                values.push({Key: currentNode.key, Value: currentNode.value});
+                currentNode = currentNode.next;
+            };
+        }
+        return values
+      };
+
+    toString(){
+        let returnString = "( " + this.head.value + " ) -> "
+        let currentNode = this.head.next
+        for(let i = 0; i < this.length - 1; i++){
+            returnString = returnString.concat("( ", currentNode.value)
+            returnString = returnString.concat(" ) -> ")
+            currentNode = currentNode.next;
+        }
+        return returnString + "null"
+    };
+
+};
+
+class Node{
+    constructor(){
+        this.key = null
+        this.value = null
+        this.next = null
+    }
+
+};
